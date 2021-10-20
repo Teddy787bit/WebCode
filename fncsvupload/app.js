cf48820fa0303c2a32df0417d70d6050fae57e5d
Pradip_Fedora <Teddy_787@yahoo.com>
2021-10-20 15:27:30 +0530

    /

--- fncsvupload/app.js
+++ fncsvupload/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 var path = require('path');
 const mongoose = require('mongoose');
-const CSV = require('./api/routes/csv');
+mongoose.Promise = global.Promise;
+const index = require('./api/routes/index');
 const app = express();
+
 mongoose.connect("mongodb://localhost:27017/medicine",{useNewUrlParser: true},(error) =>{
     if (!error){
         console.log("Done");
@@ -14,12 +16,14 @@ mongoose.connect("mongodb://localhost:27017/medicine",{useNewUrlParser: true},(e
 })
 app.listen(3000,()=> console.log("Listrning TO Port 3000"))
 
-app.use('/',CSV);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.use('/',index);
 app.get('/',(req,res)=>{
     res.render('index');
 });
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
 module.exports = app;
-
-
